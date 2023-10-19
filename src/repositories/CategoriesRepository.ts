53e56cd3f2b488885cf2662ec27f8ff5ed9a09a9
LucasSiqueiraSurreco <lucassiqueirasurreco@outlook.com>
2023-10-19 14:51:02 -0300

    validating category registration

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -22,4 +22,13 @@ export class CategoriesRepository {
 
     this.categories.push(category);
   }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category {
+    const category = this.categories.find((category) => category.name === name);
+    return category;
+  }
 }
